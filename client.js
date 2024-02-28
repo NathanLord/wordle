b2f0a7d75df7a42cc997bddbec9eaf240d587d3d
HoldenErnest <holden.ernest@icloud.com>
2024-02-27 23:17:41 -0500

    coloring for letters, and working game(somewhat)

--- client.js
+++ client.js
@@ -6,6 +6,8 @@ var currentAttempts = 0; // this will let us know what row to input a given lett
 var currentLetter = 0; // this will tell us where the next inputted letter goes
 const maxAttempts = 5;
 var wordLength = 5;
+var answer = "geode";
+var canType = true;
 
 function concatIntoWord(listOfLetters) {
     let word = "";
@@ -14,13 +16,42 @@ function concatIntoWord(listOfLetters) {
     }
     return word;
 }
+function win() {
+    // things to do if win
+        //dont let them move to the next row
+        canType = false;
+        //display congrats
+        //TODO: share score with friends maybe
+        
+}
 
+function checkWord(word) {
+    var allLetters = allwords.children[currentAttempts].children; //every span(letter)
+    for(let i = 0; i < wordLength; i++) {
+        if (word[i] == answer[i]) { //green
+            allLetters[i].style.backgroundColor = "green";
+        } else if (answer.indexOf(word[i]) != -1) { //yellow
+            allLetters[i].style.backgroundColor = "yellow";
+        } else {//grey
+            allLetters[i].style.backgroundColor = "grey";
+        }
+        
+    }
+    if (word == answer) {
+        win();
+    }
+}
 function submitWord(word) {
-    // check to make sure the user inputted at least 5 letters
-    console.log(word + " was the inputted word");
+    // check each letter and assign color
+    checkWord(word);
+    currentAttempts++;
+    currentLetter = 0;
 }
 
 function keyPress(e) {
+    if (!canType) {
+        return;
+    }
     var theKey = e.key;
     if (theKey == "Enter") { // submit all letters as a word guess
         if (currentLetter == wordLength) {
@@ -29,7 +60,6 @@ function keyPress(e) {
         }
         return;
     }
-    console.log(currentLetter);
     if (theKey == "Backspace") { // remove last letter inputted
         if (currentLetter <= 0) return;
         currentLetter--;
